34284ccebacee3d8890a22e77895ba295e63575d
Garrett Winder <garrett@erskinedesign.com>
2011-01-25 16:29:24 -0600

    created journal detail template

--- assets/templates/journal/_index_detail.php
+++ assets/templates/journal/_index_detail.php
@@ -16,8 +16,33 @@
     
     <div id="content_pri">
         
-        Journal detail
-
+        <div id="posts">
+            
+            <div class="post">
+                <h2>Album launch party on March 26</h2>
+                <ul class="post_meta">
+                    <li><time datetime="" pubdate>10-01-12</time></li>
+                    <li><a href="#">Permalink</a></li>
+                    <li><a href="#">Share on Twitter</a></li>
+                    <li><a href="#">Share on Facebook</a></li>
+                </ul>
+                <img class="img_right" src="/assets/images/content/content_pri_post1.jpg" width="253" height="164" alt="">
+                <p>
+                    <strong>The ThirtySix album launch event will take place on the 26th March 2011, at the Centenary Centre, Peel.</strong> Vivamus id mollis quam. Morbi ac iklisese commodo nulla.
+                </p>
+                <p>
+                    In condimentum orci id nisl volutpat bibendum. Quisque commodo hendrerit lorem quis egestas. Maecenas quis tortor arcu.
+                </p>
+                <p>
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus
+                </p>
+                <p>
+                    Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.
+                </p>
+            </div> <!-- // .post -->
+            
+        </div><!-- // #posts -->
+        
     </div> <!-- // #content_pri -->
     
     {embed="_layout/_sidebar"}
