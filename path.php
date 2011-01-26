2b0b41403be96786df74466c9ca7707d113c5fe1
Jamie Pittock <jamiepittock@me.com>
2011-01-26 17:41:42 +0000

    Added journal date variable

--- path.php
+++ path.php
@@ -39,9 +39,8 @@ $global_vars['pv_disable_titles']   = 'categories|custom_fields|member_data|pagi
 
 
 /* DATE FORMATTING */
-$global_vars['pv_date_short'] 		= '%F %d, %Y';
-$global_vars['pv_date_time'] 		= '%g:%i %a';
-$global_vars['pv_date_full'] 		= '%F %d %Y, %g:%i %a';
+$global_vars['pv_date_event'] 		= '%F %d, %Y';
+$global_vars['pv_date_journal'] 	= '%d-%m-%y';
 
 
 /* PAGE TYPES */
