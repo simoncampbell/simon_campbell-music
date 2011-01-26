8fcda92a3a366fdac89279f7b1cd6769b57ee5a1
Garrett Winder <garrett@erskinedesign.com>
2011-01-26 13:55:41 -0600

    html markup for contact template

--- assets/templates/contact/index.php
+++ assets/templates/contact/index.php
@@ -16,8 +16,39 @@
     
     <div id="content_pri">
         
-        Contact
-
+        <form id="contact_form" action="" autocomplete="">
+            <fieldset>
+                <ol>
+                    <li>
+                        <label for="name">Text Field <em>*</em></label><br>
+                        <input id="name" name="name" type="text">
+                    </li>
+                    <li>
+                        <label for="email">Email Address <em>*</em></label><br>
+                        <input id="email" name="email" type="text">
+                    </li>
+                    <li>
+                        <label for="message">Message <em>*</em></label><br>
+                        <textarea id="message" name="message"></textarea>
+                        <div>Total characters left: <span id="counter"></span></div>
+                    </li>
+                </ol>
+            </fieldset>
+            <input type="submit" value="Send message">
+        </form><!-- // #contact_form -->
+        <div id="form_aside">
+            <p>
+                Simon would love to hear from you. If you’d like to drop him a line, either use the form on this page or drop him a line at me@simoncampbell.com.
+            </p>
+            <p>If you want to get in touch with Simon’s agent for booking details, contact Suzy:</p>
+            <address>
+                Suzy Starlit<br>
+                Starlite Events &amp; Project Management<br>
+                suzy@starlite-events.net<br>
+                +44 7658 734 456<br>
+            </address>
+        </div><!-- // #form_aside -->
+        
     </div> <!-- // #content_pri -->
     
     {embed="_layout/_sidebar"}
