3614a503143d58fd0f42f176375460ce6184e055
Garrett Winder <garrett@erskinedesign.com>
2011-02-01 21:45:34 -0600

    IE6 home intro

--- assets/js/ie.js
+++ assets/js/ie.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     
+    $('ul#navigation_pri li:first').addClass('first');
     $('ul.item_listing li:odd').addClass('odd');
     $('ul#gallery_grid li:odd').addClass('odd');
     $('ul#navigation_pri li.account ul li:last').addClass('last');
