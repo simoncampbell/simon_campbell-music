8fa6ea4bfcb369f2153b029620e4c0f596323089
Garrett Winder <garrett@erskinedesign.com>
2011-02-01 21:24:13 -0600

    IE fixed :last-child issue on nav pri account section

--- assets/js/ie.js
+++ assets/js/ie.js
@@ -2,4 +2,6 @@ $(document).ready(function(){
     
     $('ul.item_listing li:odd').addClass('odd');
     $('ul#gallery_grid li:odd').addClass('odd');
+    $('ul#navigation_pri li.account ul li:last').addClass('last');
+    
 });
