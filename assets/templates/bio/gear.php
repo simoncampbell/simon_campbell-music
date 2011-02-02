54ed6a46e493f30b6f9ac185764461ae21aec43f
Garrett Winder <garrett@erskinedesign.com>
2011-02-02 13:33:04 -0600

    html for bio/gear template

---
+++ assets/templates/bio/gear.php
@@ -0,0 +1,57 @@
+{embed="_layout/_start"
+    body_class="editorial"
+    body_id=""
+    section="bio"
+    {!-- LG BETTER META OPTIONS BELOW --}
+    title="Gear"
+    title_suffix=""   
+    title_prefix=""   
+    description=""
+    keywords=""
+    robots_index=""
+    robots_follow=""
+    robots_archive=""
+    canonical_url="/"
+}
+    
+    <div id="content_pri">
+        <h2 class="hide">Bio</h2>
+        <div class="widget">
+            <h3>My big bastard gear list</h3>
+            <p>
+                I have over the years used a bewildering array of gear on my never ending journey to find the 'perfect tone'. If you are interested, take a look at <a href="#">the articles</a> that I have written on the subject.
+            </p>
+        </div><!-- // .widget -->
+        <div id="gear_guitars" class="widget">
+            <h3>Guitars &amp; mandolins</h3>
+            <ul>
+                <li>Fender Customshop ‘Nocaster’ relic</li>
+                <li>Custom Stratocaster with neck made by Gordon Whittam and body by Brian Eastwood out of ash with specially wound Lindy Fralin pickups. Fitted with Fender style tremolo</li>
+                <li>Custom Telecaster made by Gordon Whittam out of swamp ash with specially wound Lindy Fralin pickups</li>
+                <li>Custom Telecaster neck made by Gordon Whittam and body by Brian Eastwood out of swamp ash with specially wound Lindy Fralin humbucking pickups and Fender style tremolo system</li>
+                <li>Gibson Flying V (1974) refinished by Ted Lee, fitted with Vibrola tremolo system and specially wound Lindy Fralin humbucking pickups</li>
+                <li>Gretsch Electromatic D6 Console guitar (1956)</li>
+                <li>Howe-Orme Mandolinetto (c 1885)</li>
+                <li>Guitar Taylor GA fitted with Headway HE2/G.FEQ pickup</li>
+                <li>Collings OM2H fitted with Highlander IP2 pickup</li>
+                <li>Gibson SJ-45 Deluxe fitted with Headway HE2/G.FEQ pickup</li>
+                <li>Collings 0001 fitted with Headway HE2/G.FEQ pickup</li>
+            </ul>
+        </div><!-- // .widget -->
+        <div class="widget">
+            <h3>Amplifiers &amp; cabinets</h3>
+            <ul>
+                <li>Van Weelden Twinkleland 100W Head</li>
+                <li>Bludotone Bludo-Drive 100W Head</li>
+                <li>TwoRock 10th Anniversary 100W Head</li>
+                <li>TwoRock Sapphire 100W Head</li>
+                <li>2 x Two Rock Signature 2×12 cabinets loaded with vintage EVM12L’s</li>
+                <li>1 x Two Rock Signature 1×12 cabinet loaded with vintage EVM12L</li>
+            </ul>
+        </div><!-- // .widget -->
+        
+    </div> <!-- // #content_pri -->
+    
+    {embed="_layout/_sidebar"}
+
+{embed="_layout/_end"}
