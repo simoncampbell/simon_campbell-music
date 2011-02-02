02587415dc875fa54ef267cd27d4b3ed2a78c752
Garrett Winder <garrett@erskinedesign.com>
2011-02-01 21:59:24 -0600

    IE6 started on png fixes

--- assets/js/ie.js
+++ assets/js/ie.js
@@ -7,4 +7,7 @@ $(document).ready(function(){
     $('div#posts div.post:last').addClass('last');
     $('div#posts div.post ul.post_meta li:first').addClass('first');
     
+    
+    // IE6 PNG FIX CLASS
+    $('#branding img, div#navigation_network ul li#nn_rss a, div#navigation_network ul li#nn_twitter a, div#navigation_network ul li#nn_facebook a, div#posts div.post ul.post_meta li#pm_twitter a, div#posts div.post ul.post_meta li#pm_facebook a').addClass('png');
 });
