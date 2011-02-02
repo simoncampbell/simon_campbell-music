0f7c1d983b945bb71eeeec438c042689ca5ec01c
Garrett Winder <garrett@erskinedesign.com>
2011-02-01 21:33:27 -0600

    IE fixed :last-child issues with div.post:last-child

--- assets/js/ie.js
+++ assets/js/ie.js
@@ -3,5 +3,6 @@ $(document).ready(function(){
     $('ul.item_listing li:odd').addClass('odd');
     $('ul#gallery_grid li:odd').addClass('odd');
     $('ul#navigation_pri li.account ul li:last').addClass('last');
+    $('div#posts div.post:last').addClass('last');
     
 });
