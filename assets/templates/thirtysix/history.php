2b2dee3c78866e354be43d967d678852ec7da0c5
Garrett Winder <garrett@erskinedesign.com>
2011-02-02 14:30:04 -0600

    basic thirtysix/history html

---
+++ assets/templates/thirtysix/history.php
@@ -0,0 +1,82 @@
+{embed="_layout/_start"
+    body_class="history"
+    body_id="editorial"
+    section="thirtysix"
+    {!-- LG BETTER META OPTIONS BELOW --}
+    title="History | ThirtySix"
+    title_suffix=""   
+    title_prefix=""   
+    description=""
+    keywords=""
+    robots_index=""
+    robots_follow=""
+    robots_archive=""
+    canonical_url="/"
+}
+    
+    <div id="content_pri">
+        <h2 class="hide">ThirtySix</h2>
+        <div class="widget">
+            <h3>The journey</h3>
+            <p>
+                Well this is it, my first solo album in thirty six years of spanking the plank. It’s not blues, it’s not rock, it’s not folk; it is an eclectic mix that will take you on my very personal musical journey. Each song is written about real people, with real lives.
+            </p>
+            <p>
+                My whole band and engineering team have taken a lot of time and effort to make this sound as good as we can, using truly beautiful equipment and techniques. So, it wont be as loud as some of the CD's you buy: it’s not compressed into oblivion. It contains a lot of detail and spacial imaging, so sit down with a large glass of whatever it is that makes you smile and turn it on. Dance if you feel like it, cry if you need too.
+            </p>
+            <p>ThirtySix is an honest album and has many imperfections, but it is me, right now. I hope you enjoy it.</p>
+            <p>
+                To do my bit for the environment, we haven’t produced an insert but full detail about the songs, lyrics, musicians, the recording plus information about live shows can be found on this site.
+            </p>
+            <ul>
+                <li>Produced and arranged by <strong>Simon Campbell</strong></li>
+                <li>Engineered and co-produced by <strong>Stephen Boyce-Buckley</strong></li>
+                <li>Assistants: <strong>Viktor Sigurjónsson</strong> and <strong>Chris Taylor</strong></li>
+                <li>Recorded with love at <strong>Gracieland Studio</strong></li>
+                <li>Mastered with love by <strong>Jon Davis at Metropolis</strong></li>
+                <li>Cover graphics by <strong>Martyn Cain</strong></li>
+                <li>Photography by <strong>Phil Kneen</strong></li>
+            </ul>
+        </div><!-- // .widget -->
+        <div class="widget">
+            <h3>The journey</h3>
+            <p>
+                Steve (Dr Beat) Boyce-Buckley has been a delight to work with as he understands how to record properly, not samples, but real instruments, using the magnificent console and real outboard. He has an empathy with the artist, which is very rare indeed.
+            </p>
+            <p>
+                As co-producer, brass and backing vocal arranger, he has added many wonderful elements to the album. He has helped me to create a very honest snapshot of me and my songs. The comment this morning really floored me.
+            </p>
+            <blockquote>
+                <p>
+                    "There are very few records in the last 32 years that I have worked on that I am proud of both musically and technically. Simon Campbell’s forthcoming release is one of them and joins an elite team which can now be counted on one hand!"
+                </p>
+            </blockquote>
+            <p>
+                The studio’s ‘digital’ assistants, Viktor Sigurjónsson and Chris Taylor are delightful and engaging young men. Nothing was too much trouble. Beer, tea with Manuka honey, many trips to the mighty Co-Op.
+            </p>
+            <p>Phil (maintenance man and lawnmaster) was lovely, creating a beautiful environment for us all to work.</p>
+            <p>
+                The studio manager, Martin Rhodes gave a calm air to proceedings and managed to extract me from the pooh on numerous occasions. Never over-fussing, but there when needed.
+            </p>
+            <p>A sincere thanks to Ian Devaney and Lisa Stansfield for building and maintaining this unique, world class facility.</p>
+        </div><!-- // .widget -->
+        <div class="widget">
+            <h3>Musicians</h3>
+            <p>
+                With respect to musicians, superlatives are inappropriate as the performances speak for themselves. Their individual pedigrees would occupy many pages, but in summary they have worked with artists such as: Lisa Stansfield, Proud Mary, Barclay James Harvest, Dare, The Christians, Amy Winehouse, Ella Fitzgerald, Quincy Jones, Stevie Wonder, Paul McCartney, Dave Gilmore, Tom Jones, Robbie Williams, Oleta Adams, Paul Heaton, The Beautiful South, Simply Red, Take That, Gorilas, Heather Small, the Earlies, Hot Chip, Plan B and King Creosote. Shit&hellip;
+            </p>
+            <ul>
+                <li>Simon Campbell: vocals and guitars</li>
+                <li>Kev (Charlie) Whitehead: drums</li>
+                <li>Steve Rowe and Craig Fletcher (Bewegen and flourishes elsewhere): bass</li>
+                <li>Christian Madden: Hammond, Wurlitzer, Mellotron, Rhodes, Clavinet, Moog Voyager and a bewildering array of Moogerfoogers</li>
+                <li>Lea Mullen: percussion</li>
+                <li>The Brigadiers of Brass: Pat Hartley (Trombone), Malcolm Melling (Trumpet and Flugel Horn) and Dave Bishop on an astounding array of Saxophones.</li>
+            </ul>
+        </div><!-- // .widget -->
+        
+    </div> <!-- // #content_pri -->
+    
+    {embed="_layout/_sidebar"}
+
+{embed="_layout/_end"}
