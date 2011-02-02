65c5dd167347be8e512d29f6cffc44da00d3999f
Garrett Winder <garrett@erskinedesign.com>
2011-02-01 21:37:55 -0600

    IE6 post_meta first-child padding

--- assets/js/ie.js
+++ assets/js/ie.js
@@ -4,5 +4,6 @@ $(document).ready(function(){
     $('ul#gallery_grid li:odd').addClass('odd');
     $('ul#navigation_pri li.account ul li:last').addClass('last');
     $('div#posts div.post:last').addClass('last');
+    $('div#posts div.post ul.post_meta li:first').addClass('first');
     
 });
