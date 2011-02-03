660ab061005eb41fcc43573d171e8b2dc1f38ea6
Garrett Winder <garrett@erskinedesign.com>
2011-02-03 15:08:59 -0600

    gallery thumbs work in all browsers

--- assets/js/ie.js
+++ assets/js/ie.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     
     $('ul#navigation_pri li:first').addClass('first');
     $('ul.item_listing li:odd').addClass('odd');
-    $('ul#gallery_grid li:odd').addClass('odd');
     $('ul#navigation_pri li.account ul li:last').addClass('last');
     $('div#posts div.post:last').addClass('last');
     $('div#posts div.post ul.post_meta li:first').addClass('first');
