0a00eb8047df37e55133c1bcb74486153573df4e
Garrett Winder <garrett@erskinedesign.com>
2011-02-03 10:54:01 -0600

    IE7 & 6 fixes for newsletter template

--- assets/js/ie.js
+++ assets/js/ie.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     $('ul#navigation_pri li.account ul li:last').addClass('last');
     $('div#posts div.post:last').addClass('last');
     $('div#posts div.post ul.post_meta li:first').addClass('first');
+    $('body.newsletter div#content_pri div:first').addClass('first');
     
     // IE6 PNG FIX CLASS
     $('#branding img, div#navigation_network ul li#nn_rss a, div#navigation_network ul li#nn_twitter a, div#navigation_network ul li#nn_facebook a, div#posts div.post ul.post_meta li#pm_twitter a, div#posts div.post ul.post_meta li#pm_facebook a').addClass('png');
