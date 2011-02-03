058019691004894e17772ca1d327c59ec04ab7b0
Garrett Winder <garrett@erskinedesign.com>
2011-02-03 10:06:28 -0600

    started newsletter styling

---
+++ assets/templates/newsletter/index.php
@@ -0,0 +1,28 @@
+{embed="_layout/_start"
+    body_class="newsletter"
+    body_id=""
+    section=""
+    {!-- LG BETTER META OPTIONS BELOW --}
+    title="Newsletter"
+    title_suffix=""   
+    title_prefix=""   
+    description=""
+    keywords=""
+    robots_index=""
+    robots_follow=""
+    robots_archive=""
+    canonical_url="/"
+}
+    
+    <div id="content_pri">
+        <div class="half">
+        <h3>Thanks!</h3>
+        <p>Thanks for subscribing to the Simon Campbell Music newsletter! You'll receive all the latest news and goodies very soon!</p>
+        </div>
+        <div class="half">
+            
+        </div>
+
+    </div> <!-- // #content_pri -->
+
+{embed="_layout/_end"}
