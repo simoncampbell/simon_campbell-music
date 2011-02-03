3ddddc20f043858469f4e8c5106d5c65e96d8f0e
Garrett Winder <garrett@erskinedesign.com>
2011-02-03 15:12:39 -0600

    IE navigation bar bullet on last item removed

--- assets/js/ie.js
+++ assets/js/ie.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     
     $('ul#navigation_pri li:first').addClass('first');
     $('ul.item_listing li:odd').addClass('odd');
-    $('ul#navigation_pri li.account ul li:last').addClass('last');
+    $('ul#navigation_pri li:last').addClass('last');
     $('div#posts div.post:last').addClass('last');
     $('div#posts div.post ul.post_meta li:first').addClass('first');
     $('body.newsletter div#content_pri div:first').addClass('first');
