1619368050fb4f8164f467018fa36249b60dbac1
Garrett Winder <garrett@erskinedesign.com>
2011-02-03 12:13:12 -0600

    changed input labels on newsletter forms

--- assets/templates/store/index.php
+++ assets/templates/store/index.php
@@ -30,7 +30,7 @@
             <form class="validate_inline" action="http://erskine.createsend.com/t/r/s/bxdii/" method="post">
                 <fieldset>
                     <div>
-                        <label for="email">Email Address</label>
+                        <label for="cm-bxdii-bxdii">Email Address</label>
                         <input type="text" name="cm-bxdii-bxdii" id="bxdii-bxdii" class="required email">
                     </div>
                 </fieldset>
