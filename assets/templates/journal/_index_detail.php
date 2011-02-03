78726ae7434c25756ad769c13ba218cd9c77cb5b
Jamie Pittock <jamiepittock@me.com>
2011-02-03 15:27:48 +0000

    Added more facebook meta tags

--- assets/templates/journal/_index_detail.php
+++ assets/templates/journal/_index_detail.php
@@ -30,7 +30,9 @@
     {if weblog_short_name == "journal_audio"}
     og_audio="{cf_journal_audio_mp3}"
     og_audio_type="application/mp3"
+    og_audio_title="Testing"
     og_audio_artist="Simon Campbell"
+    og_audio_album="ThirtySix"
     {/if}
 }
     
