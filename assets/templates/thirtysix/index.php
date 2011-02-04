5fa46a07f438b3e2d20b840f306294efe83726ce
Garrett Winder <garrett@erskinedesign.com>
2011-02-04 11:50:27 -0600

    36 landing page markup changes

--- assets/templates/thirtysix/index.php
+++ assets/templates/thirtysix/index.php
@@ -58,36 +58,39 @@
         </div><!-- // #album_promo_inline -->
         
         <div class="widget" id="free_download">
-            <h3>Get <strong>&ldquo;Brother&rdquo;</strong> as a free download</h3>
-            <div>
+            <div class="first">
+                <h3>Get <strong>&ldquo;Brother&rdquo;</strong> as a free download</h3>
                 <p>Enter your email below and we&rsquo;ll send you a link to download the MP3. You&rsquo;ll also receive regular updates on the album and Simon&rsquo;s upcoming events.</p>
             </div>
-            <form class="validate_inline" action="http://erskine.createsend.com/t/r/s/bxdii/" method="post">
-                <fieldset>
-                    <div>
-                        <label for="cm-bxdii-bxdii">Your email address</label>
-                        <input type="text" name="cm-bxdii-bxdii" id="bxdii-bxdii" class="required email">
-                        <input type="submit" value="Subscribe" class="submit" />
-                    </div>
-                </fieldset>
-            </form>
+            <div class="last">
+                <form class="validate_inline" action="http://erskine.createsend.com/t/r/s/bxdii/" method="post">
+                    <fieldset>
+                        <div>
+                            <label for="cm-bxdii-bxdii">Your email address</label>
+                            <input type="text" name="cm-bxdii-bxdii" id="bxdii-bxdii" class="required email">
+                            <input type="submit" value="Subscribe" class="submit" />
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
         </div><!-- // .widget -->
         
         <div class="widget" id="journey">
-            <h3>The journey</h3>
-            <ul class="social">
-                <li class="twitter"><a href="http://twitter.com/share?url={site_url}{pv_all_segments}&amp;text=Simon%20Campbell's%20%22ThirtySix">Share on Twitter</a></li>
-                <li class="facebook">
-                    <a href="http://www.facebook.com/sharer.php?u={site_url}{pv_all_segments}">Share on Facebook</a>
-                </li>
-            </ul> <!-- // .social -->
-            <div>
+            <div class="first">
+                <h3>The journey</h3>
                 <p>
                     Well this is it, Simon&rsquo;s first solo album in thirty six years of spanking the plank. It&rsquo;s not blues, it&rsquo;s not rock, it&rsquo;s not folk; it is an eclectic mix that will take you on his very personal musical journey. Each song is written about real people, with real lives. Read all about <a href="/{segment_1}/history/">how ThirtySix came to be</a>.
                 </p>
             </div>
+            <div class="last">
+                <ul class="social">
+                    <li class="twitter"><a href="http://twitter.com/share?url={site_url}{pv_all_segments}&amp;text=Simon%20Campbell's%20%22ThirtySix">Share on Twitter</a></li>
+                    <li class="facebook">
+                        <a href="http://www.facebook.com/sharer.php?u={site_url}{pv_all_segments}">Share on Facebook</a>
+                    </li>
+                </ul><!-- // .social -->
+            </div>
         </div><!-- // .widget -->
-        
         
     </div> <!-- // #content_pri -->
     
