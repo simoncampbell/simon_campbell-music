5e9dd2612e8a84269bf8af7a55cc45c486165194
James Willock <james@erskinedesign.com>
2011-02-04 10:00:54 +0000

    Newsletter block language.

--- assets/templates/_layout/_sidebar.php
+++ assets/templates/_layout/_sidebar.php
@@ -106,7 +106,7 @@
     <div id="newsletter_signup" class="widget">
         <h2>Newsletter</h2>
         <p>
-            Sign up to receive regular updates on the album and Simon&rsquo;s upcoming events.
+            Sign up to receive regular updates on the new album and Simon&rsquo;s shows &amp; events.
         </p>
         <form action="http://erskine.createsend.com/t/r/s/bxdii/" method="post">
             <fieldset>
