acd3fefa95d70334d941331728639b4ffe1c21a2
James Willock <james@erskinedesign.com>
2011-02-04 11:34:36 +0000

    Gear page.

--- assets/templates/bio/index.php
+++ assets/templates/bio/index.php
@@ -19,52 +19,83 @@
         <h2 class="hide">Bio</h2>
         
         <div id="beginnings" class="widget">
-            <h3>Simon's musical beginnings</h3>
+            <h3>Simon&rsquo;s musical beginnings</h3>
             <p>
-                Music is my first great passion and guitars caused mayhem with my academic career: from when I first picked one up at 16, to finally scraping a bad degree in Chemistry from Salford.
+                Since strapping on his first guitar at the age of 16, Simon Campbell has spent his life immersed in music, a passion which has given him a wealth of experience across decades, countries and genres.
             </p>
             <p>
-                Whilst I was supposed to be there, I was playing with hard rockers &ldquo;Whitefire&rdquo; who released an EP, &ldquo;Parades the Glory&rdquo; in 1978. I played sessions and taught guitar during the day for a time at Bury Grammar school but unsurprisingly, didn&rsquo;t fit in with the &ldquo;staff room scene&rdquo;&hellip;
+                In the late 1970s, while still a university student in Salford, Simon furthered his musical education with hard-rockers Whitefire. The band released an EP and toured the UK before taking a &lsquo;Punk-esque R &amp; B&rsquo; direction under their new moniker, Roadrunner.
             </p>
             <p>
-                We extensively toured the UK including the legendary Deeply Vale free festival. Whitefire eventually changed direction and renamed &ldquo;Roadrunner&rdquo;, the &ldquo;punkesk&rdquo; R&amp;B band rocked its way around the country (again).
+                Returning to his Rock roots, he formed The Method with Mike Hehir from Sad Cafe and Mick Gilbourne, one time drummer with 10CC. Over a two-year period, the band toured throughout the UK and Europe.
+            </p>
+            <p>
+                
             </p>
         </div><!-- // .widget -->
         <div class="widget">
-            <h3>Changin&rsquo; up</h3>
-            <p>I remorphed my musical career forming the 80&rsquo;s europop band &ldquo;Cutting Edge&rdquo; renamed &ldquo;Little Brother&rdquo;.</p>
+            <h3>Great success</h3>
             <p>
-                Derek Lawrence (producer of Deep Purple and Wishbone Ash’s first three albums) and legendary &ldquo;Big&rdquo; Jim Sullivan signed the band on a three-album deal with Line Records (Polydor Germany). <em>Survival</em> was released in 1992. Unfortunately, for personal reasons Tim left the partnership soon after release and the band dissolved.
+                Commercial success came with his next venture, Little Brother, whose Europop sound saw them snapped up by Line Records (Polydor Germany). Although the band unfortunately split after one album (the well-received &lsquo;<a href="http://www.discogs.com/Tim-Wright-9-with-Little-Brother-Survival/release/2625575">Survival</a>&rsquo;), Simon was now free to pursue new challenges.
             </p>
             <p>
-                Seeking to return to my rock roots I formed &ldquo;the Method&rdquo; with Mike Hehir (Guitar – <em>Sad Cafe</em>) and Mick Gilbourne (Drums – <em>10cc</em>) and toured on and off for two years throughout the UK and Europe. At the same time my interest in production came to fore working with Rick Dowson (Colombia Records) and Millhause (Island Records).
+                Returning to his Rock roots, he formed The Method with Mike Hehir from Sad Cafe and Mick Gilbourne, one time drummer with 10CC. Over a two-year period, the band toured throughout the UK and Europe.
             </p>
             <p>
-                Millhause broke up and reformed as &ldquo;The Disciples&rdquo; where I joined as guitar player and lead vocalist. This band made a very well regarded &ldquo;albumette&rdquo; prompting &ldquo;Q&rdquo; to write &ldquo;Astounding presence and musicality&rdquo;. Again the band prematurely split: real shame as at this point, it was probably the best thing I had done musically.
+                During this time, Simon&rsquo;s interest in production led him to work with Rick Dowson and Millhause and, when the latter became The Disciples, Simon joined them as guitarist and lead vocalist. The Disciples disbanded in time but not before releasing a mini-album (favourably reviewed by Q), and leaving a lasting impression on live audiences.
             </p>
         </div><!-- // .widget -->
         <div id="tribute" class="widget">
-            <h3>A tribute</h3>
+            <h3>Player</h3>
             <p>
-                Just for a laugh and before &ldquo;tribute bands&rdquo; had become big, I put together an Eric Clapton tribute tour which was particularly successful.
+                Alongside his long and varied band career, Simon has also been a highly sought-after session player, collaborating with numerous high-profile musicians for both studio work and live performances.
             </p>
             <p>
-                The highlight (and funniest) was the Albert Hall's (that’s the one in Bolton!) which we packed out for two consecutive nights. Due to the intensity of my business and diving careers, I wound down playing for a while, keeping my hand in by doing a number of charity gigs for my old keyboard player Stephen Dowd at Ram Fest and also jamming regularly at the Stanley Street Club in Accrington organised with my mate Rob Livesey&hellip;
+                His <a href="http://blog.simoncampbell.com/blog/perma/the_big_move_to_the_isle_of_man/">move to the Isle of Man</a> in 2008 and subsequent involvement with its vibrant music scene led to a new a chapter in Simon&rsquo;s journey: firstly, the formation of blues-rockers The Very Very Bad Men and now, in 2011, the release of his first solo album, ThirtySix.
             </p>
             <p>
-                Moving to the Isle of Man, which has quite a vibrant music scene, has sparked guitar fever again and have put acoustic and electric bands together. Although I love almost all music, I am blues &amp; rock man at heart.
-            </p>
-        </div><!-- // .widget -->
-        <div class="widget">
-            <h3>The perfect tone</h3>
-            <p>
-                Being a totally fanatical tone monster; I deliver regular &ldquo;tone workshops&rdquo; running though my guitars, amplifiers and effects set-up to anyone who will listen!
+                The perfect showcase for Simon&rsquo;s versatility and profound musical understanding, ThirtySix harnesses a wide variety of guitar styles while being grounded in a deep-seated love of Rock and Blues.
             </p>
             <p>
-                The piles of guitars that enrage my long suffering wife, Angela, were mostly made for me by Gordon Whittam (formally of Gordon Smith, Manchester Guitar Company and Gordy) based around Fender Telecasters and use the very fine hand built Van Weelden and Bludotone amps. I do play acoustic also and again have a nice selection, the Gibson SJ45 being most treasured.
+                Whether writing, producing, recording or playing, Simon&rsquo;s appetite for musical exploration remains unabated and he intends to keep on running for a long time to come.
             </p>
         </div><!-- // .widget -->
-
+        <div id="quotes" class="widget">
+            <h3>Quotes</h3>
+            <blockquote>
+                <p>Simon Campbell, uncompromisingly true to the spirit and a lifetime of achievements, toil, woes, lows, love and highs in a single album. Does more than it says on the tin. Open and play with care. No bollocks to that, with unbridled abandon! But best off, go and see him live. Truly awesome.</p>
+                <cite>&mdash; Lenny Conroy, Triskell Promotions</cite>
+            </blockquote>
+            <blockquote>
+                <p>Kinda steely Dani-sih. Me likes.</p>
+                <cite>&mdash; Adam Curry &ndash; The Podfather!</cite>
+            </blockquote>
+            <blockquote>
+                <p>There are very few records in the last 32 years that I have worked on that I am proud of both musically and technically. Simon Campbell&rsquo;s forthcoming release is one of them and joins an elite team which can now be counted on one hand!</p>
+                <cite>&mdash; Steve Boyce-Buckley</cite>
+            </blockquote>
+            <blockquote>
+                <p>Without a doubt the most distinctive and impressive sounds from a guitar and player I have recorded. Simon is an absolute joy to work alongside&hellip;</p>
+                <cite>&mdash; Steve Boyce-Buckley</cite>
+            </blockquote>
+            <blockquote>
+                <p>Just been to Simon&rsquo;s first gig with the new band; thought it was brilliant and was going to be typical Rock Blues format. It actually felt more Rock and Jazz with some Blues in which I thought was original and fresh sounding! Band was a tight as could be! Cant wait for more gigs!</p>
+                <cite>&mdash; Matt Collister, Blues musician</cite>
+            </blockquote>
+            <blockquote>
+                <p>Simon Campbell: an awesome guitar player who sings like Robert Plant… is an understatement. So exciting to watch that everyone was bouncing off their chairs with him!</p>
+                <cite>&mdash; Carl Joughin &ndash; TalisMann Promotions</cite>
+            </blockquote>
+            <blockquote>
+                <p>I was very impressed at the gig which was not what I expected at all. The world is so full of wannabe bluesers who have pretty much put me off going out to gigs, and I&rsquo;m sorry to admit that I thought you were going to be regurgitating the same old tripe… but not at all. Proper, intelligent guitar playing that inspired me to go back home to start practicing with good, well crafted original material.</p>
+                <cite>&mdash; Mark Cleator &ndash; Manx Sound</cite>
+            </blockquote>
+            <blockquote>
+                <p>I love the blues/rock songs like Island of Rust and there&rsquo;s some nice Steeley Dan influences in some of the other tracks; top quality recording and musicianship. Rock On!</p>
+                <cite>&mdash; Lucas Campbell &ndash; DJ at <a href="http://the-rock-of-ages-radio-show.com">The Rock of Ages Radio Show</a></cite>
+            </blockquote>
+            
+        </div><!-- // .widget -->
     </div> <!-- // #content_pri -->
     
     {embed="_layout/_sidebar"}
