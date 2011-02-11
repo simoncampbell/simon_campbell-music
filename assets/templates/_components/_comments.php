2030ee5c0fc209d66cea27790597912cd2ee3d39
Jamie Pittock <jamiepittock@me.com>
2011-02-11 16:00:06 +0000

    Added boilerplate disqus code ready for styling and settings tweaks

--- assets/templates/_components/_comments.php
+++ assets/templates/_components/_comments.php
@@ -1,45 +1,25 @@
 <div id="comments">
     
-    <h2>{if embed:comment_total == "0"}Start a discussion...{if:else}Discussion{/if}</h2>
-    
-	{exp:comment:entries sort="asc" limit="20" entry_id="{embed:entry_id}"}
-    <ol id="comment_listing">
-        <li id="comment-{comment_id}">
-            <p class="comment_author"><strong>{url_as_author}</strong> {comment_date format="{pv_date_full}"}</p>
-            {comment}
- 		</li>
-    </ol>
-	{/exp:comment:entries}
-	
-    <h3>Add your comment</h3>
+    <div id="disqus_thread"></div>
+    <script type="text/javascript">
+        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+        var disqus_shortname = 'simoncampbellmusic'; // required: replace example with your forum shortname
 
-	{exp:comment:form entry_id="{embed:entry_id}"}
-	<fieldset>
+        // The following are highly recommended additional parameters. Remove the slashes in front to use.
+        var disqus_identifier = 'unique_dynamic_id_{embed:entry_id}';
+        var disqus_url = '{site_url}/journal/{segment_2}/';
+        {if ev_environment != "live"}
+        var disqus_developer = 1;
+        {/if}
 
-    	<div>
-        	<label for="comment_name">Your name:</label>
-        	<input type="text" id="comment_name" name="name" value="{name}" />
-    	</div>
-    	<div>
-        	<label for="comment_email">Email address:</label>
-        	<input type="text" id="comment_email" name="email" value="{email}" />
-    	</div>
-    	<div>
-        	<label for="comment_url">URL:</label>
-        	<input type="text" id="comment_url" name="url" value="{url}" />
-    	</div>
-
-    	<div>
-        	<label for="comment_comment">Message:</label>
-        	<textarea id="comment_comment" rows="10" cols="15" name="comment">{comment}</textarea>
-    	</div>
-    	<div>
-			<input type="hidden" name="save_info" value="yes" {save_info} />
-			<input type="hidden" name="notify_me" value="yes" {notify_me} />
-        	<input type="image" src="{pv_assets_url}/images/site/comment_form_submit.gif" />
-    	</div>
-
-	</fieldset>
-	{/exp:comment:form} 
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
     
 </div>
