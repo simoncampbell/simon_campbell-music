9512ab3995a8123114db67ce8fae12c7dc362819
Garrett Winder <garrett@erskinedesign.com>
2011-02-23 16:54:12 -0600

    browser testing store

--- assets/js/ie.js
+++ assets/js/ie.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     $('div#posts div.post:last').addClass('last');
     $('div#posts div.post ul.post_meta li:first').addClass('first');
     $('body.newsletter div#content_pri div:first').addClass('first');
-    ('dl#definition_links dd:last').addClass('last');
+    $('dl#definition_links dd:last').addClass('last');
     
     // IE6 PNG FIX CLASS
     $('#branding img, div#navigation_network ul li#nn_rss a, div#navigation_network ul li#nn_twitter a, div#navigation_network ul li#nn_facebook a, div#posts div.post ul.post_meta li.pm_twitter a, div#posts div.post ul.post_meta li.pm_facebook a').addClass('png');
