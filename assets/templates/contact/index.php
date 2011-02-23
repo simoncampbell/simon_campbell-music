78c5a320d3d85819b47e495c4243522bc52dc741
James Willock <james@erskinedesign.com>
2011-02-23 14:23:31 +0000

    Contact/form styling.

--- assets/templates/contact/index.php
+++ assets/templates/contact/index.php
@@ -16,41 +16,76 @@
     
     <div id="content_pri">
         
-        {exp:freeform:form
-            notify="mail@simoncampbell.com"
-            required="name|email|message"
-            return="contact/thanks"
-            send_user_email="yes"
-            template="contact_form"
-            user_email_template="contact_form_user"
-        }
-            <fieldset>
-                <div>
-                    <label for="name">Name</label>
-                    <input id="name" name="name" type="text">
-                </div>
-                <div>
-                    <label for="email">Email Address</label>
-                    <input id="email" name="email" type="text">
-                </div>
-                <div>
-                    <label for="message">Message</label>
-                    <textarea id="message" name="message"></textarea>
-                </div>
-            </fieldset>
-            <input class="submit" type="submit" value="Send message">
-        {/exp:freeform:form}<!-- // #contact_form -->
+        <div id="contact">
+            
+            <h2>Get in touch!</h2>
         
-        <div id="form_aside">
-        {if segment_2 == 'thanks'}
-            {lv_contact_form_thanks}
-        {if:elseif segment_2 == 'subscribed'}
-            {lv_contact_newsletter_thanks}
-        {if:else}
-            {lv_contact_aside}
-            <p>{lv_contact_presskit_text}</p>
-        {/if}
-        </div><!-- // #form_aside -->
+            {if segment_2 == 'thanks'}
+                {lv_contact_form_thanks}
+            {if:elseif segment_2 == 'subscribed'}
+                {lv_contact_newsletter_thanks}
+            {if:else}
+                {lv_contact_aside}    
+            {/if}
+        
+            {exp:freeform:form
+                notify="mail@simoncampbell.com"
+                required="name|email|message"
+                return="contact/thanks"
+                send_user_email="yes"
+                template="contact_form"
+                user_email_template="contact_form_user"
+            }
+                <fieldset>
+                    <div>
+                        <label for="name">Your name</label>
+                        <input id="name" name="name" type="text" />
+                    </div>
+                    <div>
+                        <label for="email">Email address</label>
+                        <input id="email" name="email" type="text" />
+                    </div>
+                    <div>
+                        <label for="message">Your message</label>
+                        <textarea id="message" name="message"></textarea>
+                    </div>
+                </fieldset>
+                <input class="submit" type="submit" value="Send message" />
+            {/exp:freeform:form}<!-- // #contact_form -->
+    
+
+        
+        </div> <!-- // #contact -->
+        
+        <div class="contact_block">
+            
+            <h2>Bookings</h2>
+            
+            <p>If you want to get in touch with Simon&rsquo;s record company for booking details, contact Suzy:</p>
+            <address>
+                <strong>Suzy Starlite</strong><br />
+                Supertone Records<br />
+                <a href="mailto:suzy@supertonerecords.com">suzy@supertonerecords.com</a><br />
+                +44 7624 245881<br />
+            </address>
+        </div> <!-- // #media -->
+        
+        <div class="contact_block">
+            
+            <h2>Media</h2>
+            <p>You press and media types may be interested in the following:</p>
+            <ul>
+                <li>
+                    <a href="#">Media kit</a> <span class="pdf">5mb PDF</span>
+                </li>
+            </ul>
+            
+            {!--
+            {if lv_contact_presskit_text != ""}
+                <p>{lv_contact_presskit_text}</p>
+            {/if}
+            --}
+        </div> <!-- // #media -->
         
     </div> <!-- // #content_pri -->
     
