456a07673c32b94512223b5a4aa2c03fbc25acb4
Garrett Winder <garrett@erskinedesign.com>
2011-03-25 12:18:10 -0500

    added 'all' to category list on product listing pages

--- assets/templates/store/_tshirts_list.php
+++ assets/templates/store/_tshirts_list.php
@@ -20,6 +20,7 @@
         
         <dl id="definition_links" class="horizontal">
             <dt>Categories</dt>
+            <dd><a href="{pv_site_url}/store/">All</a></dd>
             <dd><a href="{pv_site_url}/store/music/">Music</a></dd>
             <dd><a href="{pv_site_url}/store/posters/">Posters</a></dd>
             <dd class="cur"><a href="{pv_site_url}/store/tshirts/">T-shirts</a></dd>
