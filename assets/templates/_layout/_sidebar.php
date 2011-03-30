fec3b39b209cf1207b1acbd45ee67453418ea45e
Garrett Winder <garrett@erskinedesign.com>
2011-03-30 13:53:42 -0500

    templated sidebar 'from the store' functionality

--- assets/templates/_layout/_sidebar.php
+++ assets/templates/_layout/_sidebar.php
@@ -85,43 +85,60 @@
         {/if}
     {/exp:weblog:entries}
     
+    {if lv_featured_sidebar_store != ""}    
     <div id="store_block" class="widget">
         <h2>From the store</h2>
-        <a href="{pv_site_url}/store/tshirts/thirtysix-t-shirts/">
-            <img src="{pv_assets_url}/images/content/widget_store.jpg" width="268" height="230" alt="ThirtySix album t-shirt">
+        {exp:weblog:entries
+            weblog="{weblog_short_name}"
+            limit="1"
+            entry_id="{lv_featured_sidebar_store}"
+            disable="member_data|trackbacks|categories"
+            orderby=""
+            sort=""
+            dynamic="off"
+        }
+        {if weblog_short_name == 'products_music'}  
+        <a href="{pv_site_url}/store/music/{url_title}/">
+            {cf_products_music_images limit="1"}
+            {exp:ed_imageresizer 
+                maxWidth="268"
+                forceWidth="yes"
+                image="{ffm_images_image}"
+                alt=""
+            }
+            {/cf_products_music_images}
+            <small>{title}</small>
         </a>
-        {!--
-        {if logged_in}
-        <h2>Basket</h2>
-        {exp:cartthrob:cart_items_info}
-            {if no_results}
-                <p>There is nothing in your basket</p>
-            {/if}
-            {if first_row}
-                <ul class="item_listing">
-            {/if}
-                    <li>
-                        <a href="#">
-                            <h3>{title}{if {quantity} > 1} ({quantity}){/if}</h3>
-                            <p><strong>{item_subtotal}</strong></p>
-                        </a>
-                    </li>
-            {if last_row}
-                    <li>
-                        <a href="#">
-                            <h3>Sub-total</h3>
-                            <p><strong>{cart_subtotal}</strong> + shipping</p>
-                        </a>
-                    </li>
-                </ul>
-                <p class="more">
-                    <span class="hide">View </span><a href="/store/basket/">Your basket</a> <span class="hide">or </span><a class="button" href="/store/checkout/">Go to checkout &rsaquo;&rsaquo;</a>
-                </p>
-            {/if}
-        {/exp:cartthrob:cart_items_info}
-        {/if}
-        --}
+        {/if}   
+        {if weblog_short_name == 'products_posters'}    
+        <a href="{pv_site_url}/store/posters/{url_title}/">
+            {cf_products_posters_images limit="1"}
+            {exp:ed_imageresizer 
+                maxWidth="268"
+                forceWidth="yes"
+                image="{ffm_images_image}"
+                alt=""
+            }
+            {/cf_products_posters_images}
+            <small>{title}</small>
+        </a>
+        {/if}   
+        {if weblog_short_name == 'products_tshirts'}    
+        <a href="{pv_site_url}/store/tshirts/{url_title}/">
+            {cf_products_tshirts_images limit="1"}
+            {exp:ed_imageresizer 
+                maxWidth="268"
+                forceWidth="yes"
+                image="{ffm_images_image}"
+                alt=""
+            }
+            {/cf_products_tshirts_images}
+            <small>{title}</small>
+        </a>
+        {/if}   
+        {/exp:weblog:entries}
     </div> <!-- // #store_block -->
+    {/if}   
     
     <div id="payments_block" class="widget">
         <h2>Payments</h2>
